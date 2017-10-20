6d8780dcd5286b02bc219201609bbe96935a7f19
greg-bahr <greg.bahr123@gmail.com>
2017-10-20 10:58:57 -0500

    setup canvas using JSMpeg

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+declare var JSMpeg: any;
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements AfterViewInit {
 
-  constructor() { }
+  private player: any;
+  @ViewChild('player') canvas;
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.player = new JSMpeg.Player('ws://127.0.0.1:3002', { canvas: this.canvas.nativeElement });
   }
-
 }
